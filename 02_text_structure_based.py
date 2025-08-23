02483c8a112c5756eddf80b03bb135a6897b6a20
Waqar Ali <waqar0343930@gmail.com>
2025-08-23 12:19:27 +0500

    text structure based text splitting

---
+++ 02_text_structure_based.py
@@ -0,0 +1,16 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("dl-curriculum.pdf")
+
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=0
+)
+
+result = text_splitter.split_documents(docs)
+
+print(result[0].page_content)
+print(len(result))
